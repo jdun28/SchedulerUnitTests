3185b2e0e3f66867d7b4c072c8a149a0aa315523
jdun28 <64497101+jdun28@users.noreply.github.com>
2020-12-03 09:54:04 -0800

    Initial commit

---
+++ SchedulerUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SchedulerUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SchedulerUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ba7a117d-4897-4d77-9008-0c90d7a4fa47")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SchedulerUnitTests/StringFormatTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScheduleProgram.Universal;
+using System;
+using System.IO;
+using System.Data;
+
+
+namespace SchedulerUnitTests
+{
+    [TestClass]
+    public class StringFormatTests
+    {
+
+        Universals universal = new Universals();
+        public bool ValidateField(bool isValid)
+        {
+            if (isValid)
+            {
+                return true;
+            }
+            else
+            {
+                Console.Write("Entered string does not match the required format");
+                return false;
+            }
+        }
+
+        [TestMethod]
+        public void ValidPhoneFormatTest()
+        {
+            //verify valid phone number is accepted via this method.
+            string validPhone = "555-8675";
+            bool validPhoneFormat = universal.CheckPhoneFormat(validPhone);
+            ValidateField(validPhoneFormat);
+
+        }
+
+        [TestMethod]
+        public void InvalidPhoneFormatTest()
+        {
+            //expect method validation to fail with invalidPhone
+            string invalidPhone = "4444-444";
+            bool invalidPhoneFormat = universal.CheckPhoneFormat(invalidPhone);
+            ValidateField(invalidPhoneFormat);
+        }
+
+        [TestMethod]
+        public void ValidZipFormatTest()
+        {
+            string validZip = "11111";
+            bool validZipFormat = universal.CheckZipFormat(validZip);
+            ValidateField(validZipFormat);
+        }
+
+        [TestMethod]
+        public void InvalidZipFormatTest()
+        {
+            string invalidZip = "aaaaa";
+            bool invalidZipFormat = universal.CheckZipFormat(invalidZip);
+            ValidateField(invalidZipFormat);
+        }
+    }
+}
